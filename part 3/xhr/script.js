3a00132b1aaac826fdb80eb09706733a70ecfa28
ahmadraza.fsd.pk94@gmail.com <ahmadraza.fsd.pk94@gmail.com>
2023-08-10 12:39:15 +0500

    Part 3

---
+++ part 3/xhr/script.js
@@ -0,0 +1,70 @@
+const URL="https://jsonplaceholder.typicode.com/posts"
+const xhr=new XMLHttpRequest
+xhr.open("GET",URL)
+xhr.onload=function () {
+    const response=xhr.response
+    const data=JSON.parse(response)
+    console.log(data);
+}
+xhr.send()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// xhr.open("GET",URL)
+// xhr.onload=function () {
+//     const response=xhr.response
+//     const data= JSON.parse(response)
+//     console.log(data);
+// }
+// xhr.send()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
